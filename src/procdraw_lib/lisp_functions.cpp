72613ec733dddd8c7eecb9d81938a7e80b5700e8
Simon Bates <simon@bitstructures.com>
2014-11-29 09:16:10 -0500

    Try out some C macros to reduce duplicated code

--- src/procdraw_lib/lisp_functions.cpp
+++ src/procdraw_lib/lisp_functions.cpp
@@ -1,10 +1,19 @@
 #include "lisp_functions.h"
 
+#define FOR_EACH(L, n, lst) \
+    for (LispObjectPtr n = lst; !L->Null(n); n = L->Cdr(n))
+
+#define REDUCE_NUM(L, acc, x, exp, lst) \
+    FOR_EACH(L, REDUCE_NUM_n, lst) { \
+        auto x = L->NumVal(L->Car(REDUCE_NUM_n)); \
+        acc = exp; \
+    }
+
 namespace procdraw {
 
     LispObjectPtr lisp_Apply(LispInterpreter *L, LispObjectPtr args, LispObjectPtr env)
     {
-        // TODO check number of args
+        // TODO check number of args?
         return L->Apply(L->Car(args), L->Cadr(args), env);
     }
 
@@ -25,21 +34,14 @@ namespace procdraw {
         }
 
         // 2 or more args
-        while (!L->Null(n)) {
-            difference -= L->NumVal(L->Car(n));
-            n = L->Cdr(n);
-        }
+        REDUCE_NUM(L, difference, x, difference - x, n);
         return L->MakeNumber(difference);
     }
 
     LispObjectPtr lisp_Product(LispInterpreter *L, LispObjectPtr args, LispObjectPtr env)
     {
         double product = 1;
-        LispObjectPtr n = args;
-        while (!L->Null(n)) {
-            product *= L->NumVal(L->Car(n));
-            n = L->Cdr(n);
-        }
+        REDUCE_NUM(L, product, x, product * x, args);
         return L->MakeNumber(product);
     }
 
@@ -60,21 +62,14 @@ namespace procdraw {
         }
 
         // 2 or more args
-        while (!L->Null(n)) {
-            quotient /= L->NumVal(L->Car(n));
-            n = L->Cdr(n);
-        }
+        REDUCE_NUM(L, quotient, x, quotient / x, n);
         return L->MakeNumber(quotient);
     }
 
     LispObjectPtr lisp_Sum(LispInterpreter *L, LispObjectPtr args, LispObjectPtr env)
     {
         double sum = 0;
-        LispObjectPtr n = args;
-        while (!L->Null(n)) {
-            sum += L->NumVal(L->Car(n));
-            n = L->Cdr(n);
-        }
+        REDUCE_NUM(L, sum, x, sum + x, args);
         return L->MakeNumber(sum);
     }
 
