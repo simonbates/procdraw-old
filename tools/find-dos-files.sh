a3b69c04a957bd618bea8f1b6375ff390cb47a56
Simon Bates <simon@bitstructures.com>
2015-03-29 14:55:11 -0400

    Ignore "build" in tools/find-dos-files.sh

--- tools/find-dos-files.sh
+++ tools/find-dos-files.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-find . -name .git -prune -o -type f -print0 | xargs -0 grep -l $(printf '\r')
+find . -name .git -prune \
+    -o -name build -prune \
+    -o -type f -print0 \
+    | xargs -0 grep -l $(printf '\r')
