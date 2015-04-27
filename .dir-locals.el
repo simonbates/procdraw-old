53eca0cc96174bfff3fe63a19798598e18e1de6b
Simon Bates <simon@bitstructures.com>
2015-04-27 08:32:28 -0400

    Use CTest

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,8 @@
 ((nil . ((eval . (progn
                    (when (require 'projectile nil t)
                      (puthash (projectile-project-root)
-                              "make -C build --no-print-directory"
+                              "cd build && make"
                               projectile-compilation-cmd-map)
                      (puthash (projectile-project-root)
-                              "make -C build --no-print-directory test"
+                              "cd build && ctest --output-on-failure"
                               projectile-test-cmd-map)))))))
