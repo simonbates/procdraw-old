4d6e993b9acfaae77d602dd664194350dc4e7060
Simon Bates <simon@bitstructures.com>
2015-10-03 13:03:13 -0400

    Add expected number of tests and rename test to example

--- tests/function_spec_tests/test_function_spec.cc
+++ tests/function_spec_tests/test_function_spec.cc
@@ -1,26 +1,28 @@
 #include "lisp_interpreter.h"
 #include <libxml/xmlreader.h>
 #include <iostream>
+#include <stdlib.h>
 
-static bool DoTest(xmlTextReaderPtr reader, procdraw::LispInterpreter *L)
+static bool TestExample(xmlTextReaderPtr reader, procdraw::LispInterpreter *L, int *numTests)
 {
     bool testPassed = false;
 
     xmlChar *expr = xmlTextReaderGetAttribute(reader, BAD_CAST "expr");
     if (expr != NULL) {
-        xmlChar *expected = xmlTextReaderGetAttribute(reader, BAD_CAST "expected");
-        if (expected != NULL) {
+        xmlChar *expectedValue = xmlTextReaderGetAttribute(reader, BAD_CAST "value");
+        if (expectedValue != NULL) {
             std::string result = L->PrintToString(L->Eval(L->Read(reinterpret_cast<char*>(expr))));
-            if (xmlStrEqual(expected, BAD_CAST result.c_str())) {
+            *numTests = *numTests + 1;
+            if (xmlStrEqual(expectedValue, BAD_CAST result.c_str())) {
                 testPassed = true;
             }
             else {
                 testPassed = false;
                 std::cerr << "EXPR: " << expr;
-                std::cerr << " EXPECTED: " << expected;
+                std::cerr << " EXPECTED: " << expectedValue;
                 std::cerr << " ACTUAL: " << result << std::endl;
             }
-            xmlFree(expected);
+            xmlFree(expectedValue);
         }
         xmlFree(expr);
     }
@@ -28,7 +30,7 @@ static bool DoTest(xmlTextReaderPtr reader, procdraw::LispInterpreter *L)
     return testPassed;
 }
 
-static bool TestFunction(const char *filename)
+static bool TestFunction(const char *filename, int *numTests)
 {
     procdraw::LispInterpreter L;
     xmlTextReaderPtr reader = xmlReaderForFile(filename, NULL, 0);
@@ -43,8 +45,8 @@ static bool TestFunction(const char *filename)
     int ret = xmlTextReaderRead(reader);
     while (ret == 1) {
         const xmlChar *name = xmlTextReaderConstName(reader);
-        if (name != NULL && xmlStrEqual(name, BAD_CAST "test")) {
-            if (!DoTest(reader, &L)) {
+        if (name != NULL && xmlStrEqual(name, BAD_CAST "example")) {
+            if (!TestExample(reader, &L, numTests)) {
                 allPassed = false;
             }
         }
@@ -58,15 +60,21 @@ static bool TestFunction(const char *filename)
 
 int main(int argc, char **argv)
 {
-    if (argc != 2) {
+    if (argc != 3) {
         return 1;
     }
 
     LIBXML_TEST_VERSION
 
-    bool passed = TestFunction(argv[1]);
+    int numTests = 0;
+    bool passed = TestFunction(argv[1], &numTests);
 
     xmlCleanupParser();
+
+    if (atoi(argv[2]) != numTests) {
+        std::cerr << "EXPECTED " << argv[2] << " TESTS BUT " << numTests << " WERE RUN" << std::endl;
+        return 1;
+    }
 
     return (passed ? 0 : 1);
 }
