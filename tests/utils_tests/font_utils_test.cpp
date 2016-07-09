78668f648649331186f0be83493a805f58be3d4f
Simon Bates <simon@bitstructures.com>
2016-07-09 18:31:27 -0400

    Convert tests to Google Test

---
+++ tests/utils_tests/font_utils_test.cpp
@@ -0,0 +1,162 @@
+#include "font_utils.h"
+#include "gtest/gtest.h"
+#include <sstream>
+
+class FontUtilsTest : public ::testing::Test {
+protected:
+    virtual void SetUp() {
+        fontMetrics_ = MakeTestFontMetrics();
+        exclamationCoords_ = procdraw::LayOutGlyph(fontMetrics_.GetGlyph(33), fontMetrics_);
+        quoteCoords_ = procdraw::LayOutGlyph(fontMetrics_.GetGlyph(34), fontMetrics_);
+        spaceAdvance_ = fontMetrics_.GetGlyph(32).AdvanceWidthPixels;
+        exclamationAdvance_ = fontMetrics_.GetGlyph(33).AdvanceWidthPixels;
+        quoteAdvance_ = fontMetrics_.GetGlyph(34).AdvanceWidthPixels;
+    }
+
+    static procdraw::TextureFontMetrics MakeTestFontMetrics() {
+        procdraw::TextureFontMetrics metrics;
+
+        metrics.AscenderPixels = 32;
+        metrics.DescenderPixels = -8;
+        metrics.LinespacePixels = 48;
+        metrics.TextureWidth = 128;
+        metrics.TextureHeight = 64;
+
+        metrics.ClearGlyphs(34);
+
+        procdraw::TextureGlyphMetrics space;
+        space.XoffsetPixels = 0;
+        space.WidthPixels = 0;
+        space.HeightPixels = 0;
+        space.AdvanceWidthPixels = 30;
+        space.LeftBearingPixels = 0;
+        space.TopBearingPixels = 0;
+        metrics.SetGlyph(32, space);
+
+        procdraw::TextureGlyphMetrics exclamation;
+        exclamation.XoffsetPixels = 0;
+        exclamation.WidthPixels = 14;
+        exclamation.HeightPixels = 28;
+        exclamation.AdvanceWidthPixels = 18;
+        exclamation.LeftBearingPixels = 2;
+        exclamation.TopBearingPixels = 26;
+        metrics.SetGlyph(33, exclamation);
+
+        procdraw::TextureGlyphMetrics quote;
+        quote.XoffsetPixels = 32;
+        quote.WidthPixels = 16;
+        quote.HeightPixels = 12;
+        quote.AdvanceWidthPixels = 24;
+        quote.LeftBearingPixels = 4;
+        quote.TopBearingPixels = 20;
+        metrics.SetGlyph(34, quote);
+
+        return metrics;
+    }
+
+    static void ExpectGlyphVertices(const procdraw::GlyphCoords &expected,
+                                    int expectedHOffset, int expectedVOffset,
+                                    const std::vector<float> &vertices, int verticesOffset) {
+
+        std::ostringstream message;
+        message << "verticesOffset=" << verticesOffset;
+        SCOPED_TRACE(message.str());
+
+        // Left top
+        EXPECT_EQ(expectedHOffset + expected.Left,   vertices[verticesOffset + 0]);
+        EXPECT_EQ(expectedVOffset + expected.Top,    vertices[verticesOffset + 1]);
+        EXPECT_EQ(expected.TextureLeft,              vertices[verticesOffset + 2]);
+        EXPECT_EQ(expected.TextureTop,               vertices[verticesOffset + 3]);
+
+        // Left bottom
+        EXPECT_EQ(expectedHOffset + expected.Left,   vertices[verticesOffset + 4]);
+        EXPECT_EQ(expectedVOffset + expected.Bottom, vertices[verticesOffset + 5]);
+        EXPECT_EQ(expected.TextureLeft,              vertices[verticesOffset + 6]);
+        EXPECT_EQ(expected.TextureBottom,            vertices[verticesOffset + 7]);
+
+        // Right top
+        EXPECT_EQ(expectedHOffset + expected.Right,  vertices[verticesOffset + 8]);
+        EXPECT_EQ(expectedVOffset + expected.Top,    vertices[verticesOffset + 9]);
+        EXPECT_EQ(expected.TextureRight,             vertices[verticesOffset + 10]);
+        EXPECT_EQ(expected.TextureTop,               vertices[verticesOffset + 11]);
+
+        // Left bottom
+        EXPECT_EQ(expectedHOffset + expected.Left,   vertices[verticesOffset + 12]);
+        EXPECT_EQ(expectedVOffset + expected.Bottom, vertices[verticesOffset + 13]);
+        EXPECT_EQ(expected.TextureLeft,              vertices[verticesOffset + 14]);
+        EXPECT_EQ(expected.TextureBottom,            vertices[verticesOffset + 15]);
+
+        // Right bottom
+        EXPECT_EQ(expectedHOffset + expected.Right,  vertices[verticesOffset + 16]);
+        EXPECT_EQ(expectedVOffset + expected.Bottom, vertices[verticesOffset + 17]);
+        EXPECT_EQ(expected.TextureRight,             vertices[verticesOffset + 18]);
+        EXPECT_EQ(expected.TextureBottom,            vertices[verticesOffset + 19]);
+
+        // Right top
+        EXPECT_EQ(expectedHOffset + expected.Right,  vertices[verticesOffset + 20]);
+        EXPECT_EQ(expectedVOffset + expected.Top,    vertices[verticesOffset + 21]);
+        EXPECT_EQ(expected.TextureRight,             vertices[verticesOffset + 22]);
+        EXPECT_EQ(expected.TextureTop,               vertices[verticesOffset + 23]);
+    }
+
+    procdraw::TextureFontMetrics fontMetrics_;
+    procdraw::GlyphCoords exclamationCoords_;
+    procdraw::GlyphCoords quoteCoords_;
+    int spaceAdvance_;
+    int exclamationAdvance_;
+    int quoteAdvance_;
+};
+
+TEST_F(FontUtilsTest, LayOutGlyph)
+{
+    EXPECT_EQ(2, exclamationCoords_.Left);
+    EXPECT_EQ(16, exclamationCoords_.Right);
+    EXPECT_EQ(6, exclamationCoords_.Top);
+    EXPECT_EQ(34, exclamationCoords_.Bottom);
+    EXPECT_EQ(0.0f, exclamationCoords_.TextureLeft);
+    EXPECT_EQ(0.109375f, exclamationCoords_.TextureRight);
+    EXPECT_EQ(0.0f, exclamationCoords_.TextureTop);
+    EXPECT_EQ(0.4375f, exclamationCoords_.TextureBottom);
+
+    EXPECT_EQ(4, quoteCoords_.Left);
+    EXPECT_EQ(20, quoteCoords_.Right);
+    EXPECT_EQ(12, quoteCoords_.Top);
+    EXPECT_EQ(24, quoteCoords_.Bottom);
+    EXPECT_EQ(0.25f, quoteCoords_.TextureLeft);
+    EXPECT_EQ(0.375f, quoteCoords_.TextureRight);
+    EXPECT_EQ(0.0f, quoteCoords_.TextureTop);
+    EXPECT_EQ(0.1875f, quoteCoords_.TextureBottom);
+}
+
+TEST_F(FontUtilsTest, TextLayoutForEmptyStringHasOneEmptyLine)
+{
+    auto layout = procdraw::LayOutText<float>("", fontMetrics_, 100, 100);
+
+    EXPECT_EQ(spaceAdvance_, layout.FixedGlyphWidth);
+    EXPECT_EQ(40, layout.GlyphHeight);
+    EXPECT_EQ(fontMetrics_.LinespacePixels, layout.LinespacePixels);
+    EXPECT_EQ(100, layout.MaxLineWidthPixels);
+
+    EXPECT_EQ(1, layout.NumLines());
+    EXPECT_EQ(0, layout.GetNumCharsInLine(0));
+    EXPECT_EQ(0, layout.GetVerticesForLine(0).size());
+}
+
+TEST_F(FontUtilsTest, TextLayoutForSingleLine)
+{
+    auto layout = procdraw::LayOutText<float>(" ! \"", fontMetrics_, 10, 1000);
+
+    EXPECT_EQ(spaceAdvance_, layout.FixedGlyphWidth);
+    EXPECT_EQ(40, layout.GlyphHeight);
+    EXPECT_EQ(fontMetrics_.LinespacePixels, layout.LinespacePixels);
+    EXPECT_EQ(1000, layout.MaxLineWidthPixels);
+
+    EXPECT_EQ(1, layout.NumLines());
+
+    EXPECT_EQ(4, layout.GetNumCharsInLine(0));
+
+    auto vertices = layout.GetVerticesForLine(0);
+    EXPECT_EQ(24 * 2, vertices.size());
+    ExpectGlyphVertices(exclamationCoords_, spaceAdvance_, 0, vertices, 0);
+    ExpectGlyphVertices(quoteCoords_, spaceAdvance_ * 2 + exclamationAdvance_, 0, vertices, 24);
+}
