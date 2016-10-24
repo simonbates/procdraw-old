8784b3e0ece13365c7641c0e01f3c5f9ea1bebc2
Simon Bates <simon@bitstructures.com>
2016-10-24 16:21:45 -0400

    Use explicit TEST_F for FunctionSpecsTest
    I was previously using a more data-driven approach with
    TestWithParam<SpecInfo> but I don't like that the test run output doesn't
    include the name of the function under test. Just
    "CheckFunctionSpecs/FunctionSpecsTest.Check/<N>"

--- src/procdraw_core_tests/function_specs_test.cpp
+++ src/procdraw_core_tests/function_specs_test.cpp
@@ -1,31 +1,22 @@
 #include "procdraw_core_tests/function_spec_checker.h"
 #include "gtest/gtest.h"
 #include <string>
-#include <utility>
 
-typedef std::pair<std::string, int> SpecInfo;
-
-// *********************************************************
-// BEGIN FUNCTION SPECS
-// *********************************************************
-
-static SpecInfo functionSpecs[] = {
-  SpecInfo("clear.xml", 5),      SpecInfo("cons.xml", 3),
-  SpecInfo("difference.xml", 5), SpecInfo("eq.xml", 7),
-  SpecInfo("functionp.xml", 4),  SpecInfo("get.xml", 5),
-  SpecInfo("keys.xml", 4),       SpecInfo("make-dict.xml", 5),
-  SpecInfo("memb.xml", 6),       SpecInfo("product.xml", 5),
-  SpecInfo("put.xml", 5),        SpecInfo("quotient.xml", 4),
-  SpecInfo("sum.xml", 5)
-};
-
-// *********************************************************
-// END FUNCTION SPECS
-// *********************************************************
-
-class FunctionSpecsTest : public ::testing::TestWithParam<SpecInfo>
+class FunctionSpecsTest : public ::testing::Test
 {
 protected:
+  void CheckSpec(const std::string& filename, int expectedNumTests)
+  {
+    bool passed = checker_.Check(ResolveSpecFile(filename).c_str(),
+                                 expectedNumTests);
+    EXPECT_TRUE(passed);
+    if (!passed) {
+      for (auto message : checker_.GetMessages()) {
+        ADD_FAILURE() << message;
+      }
+    }
+  }
+
   static std::string ResolveSpecFile(const std::string& filename)
   {
     return PROCDRAW_FUNCTION_SPECS_DIR + std::string("/") + filename;
@@ -34,17 +25,67 @@ protected:
   procdraw_test::FunctionSpecChecker checker_;
 };
 
-TEST_P(FunctionSpecsTest, Check)
+TEST_F(FunctionSpecsTest, Clear)
 {
-  bool passed = checker_.Check(ResolveSpecFile(GetParam().first).c_str(),
-                               GetParam().second);
-  EXPECT_TRUE(passed);
-  if (!passed) {
-    for (auto message : checker_.GetMessages()) {
-      ADD_FAILURE() << message;
-    }
-  }
+  CheckSpec("clear.xml", 5);
 }
 
-INSTANTIATE_TEST_CASE_P(CheckFunctionSpecs, FunctionSpecsTest,
-                        ::testing::ValuesIn(functionSpecs));
+TEST_F(FunctionSpecsTest, Cons)
+{
+  CheckSpec("cons.xml", 3);
+}
+
+TEST_F(FunctionSpecsTest, Difference)
+{
+  CheckSpec("difference.xml", 5);
+}
+
+TEST_F(FunctionSpecsTest, Eq)
+{
+  CheckSpec("eq.xml", 7);
+}
+
+TEST_F(FunctionSpecsTest, Functionp)
+{
+  CheckSpec("functionp.xml", 4);
+}
+
+TEST_F(FunctionSpecsTest, Get)
+{
+  CheckSpec("get.xml", 5);
+}
+
+TEST_F(FunctionSpecsTest, Keys)
+{
+  CheckSpec("keys.xml", 4);
+}
+
+TEST_F(FunctionSpecsTest, MakeDict)
+{
+  CheckSpec("make-dict.xml", 5);
+}
+
+TEST_F(FunctionSpecsTest, Memb)
+{
+  CheckSpec("memb.xml", 6);
+}
+
+TEST_F(FunctionSpecsTest, Product)
+{
+  CheckSpec("product.xml", 5);
+}
+
+TEST_F(FunctionSpecsTest, Put)
+{
+  CheckSpec("put.xml", 5);
+}
+
+TEST_F(FunctionSpecsTest, Quotient)
+{
+  CheckSpec("quotient.xml", 4);
+}
+
+TEST_F(FunctionSpecsTest, Sum)
+{
+  CheckSpec("sum.xml", 5);
+}
