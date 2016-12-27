7ce1afdaed566f087ebf3c4bdfca171f33fbc8c7
Simon Bates <simon@bitstructures.com>
2016-12-27 10:53:30 +0000

    Update vagrant build to use make check

--- vagrantfiles/trusty64/install_user_files.sh
+++ vagrantfiles/trusty64/install_user_files.sh
@@ -7,7 +7,7 @@ EOF
 cat > "$HOME/make_test_procdraw.sh" << "EOF"
 #!/bin/sh
 cd $HOME && rm -rf procdraw_build && mkdir procdraw_build
-cd procdraw_build && cmake /procdraw && make && ctest -V
+cd procdraw_build && cmake /procdraw && make && make check
 EOF
 
 chmod u+x "$HOME/make_test_procdraw.sh"
