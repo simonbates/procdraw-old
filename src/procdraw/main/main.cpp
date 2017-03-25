a64fb527efedb6427ec3a95ed77edb3f80fa16c3
Simon Bates <simon@bitstructures.com>
2017-03-25 14:14:59 -0400

    Make OpenGL required again

--- src/procdraw/main/main.cpp
+++ src/procdraw/main/main.cpp
@@ -1,49 +1,17 @@
 #define SDL_MAIN_HANDLED
 
-#ifdef PROCDRAW_ENABLE_OPENGL
-#include "procdraw/opengl/procdraw_app.h"
-#include "procdraw/opengl/sdl_util.h"
-#endif
-
 #ifdef _WIN32
 #include <windows.h>
 #endif
 
-#include "procdraw/core/interpreter/lisp_interpreter.h"
-#include <iostream>
-#include <string>
+#include "procdraw/opengl/procdraw_app.h"
+#include "procdraw/opengl/sdl_util.h"
 
-int RunSuperSimpleRepl()
-{
-    procdraw::LispInterpreter interpreter;
-    std::string expr;
-    while (true) {
-        std::cout << "> ";
-        std::getline(std::cin, expr);
-        // TODO: Note the hardcoded 40 char width
-        std::cout << interpreter.PrettyPrintToString(
-                         interpreter.Eval(interpreter.Read(expr)), 40)
-                  << std::endl;
-    }
-    return 0;
-}
-
-#ifdef PROCDRAW_ENABLE_OPENGL
-int RunGui()
+int main()
 {
     procdraw::SDL sdl(SDL_INIT_VIDEO);
     procdraw::ProcdrawApp app;
     return app.MainLoop();
-}
-#endif
-
-int main()
-{
-#ifdef PROCDRAW_ENABLE_OPENGL
-    return RunGui();
-#else
-    return RunSuperSimpleRepl();
-#endif
 }
 
 #ifdef _WIN32
