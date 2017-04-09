d084133243fbf4474bc6b4a2dfd84a19926cbc76
Simon Bates <simon@bitstructures.com>
2017-04-09 10:47:26 -0400

    Introduce a Python module for the scripts

--- scripts/lint.py
+++ scripts/lint.py
@@ -1,26 +1,5 @@
-import itertools, os, subprocess, sys
+import procdraw
 
-def find(*args):
-    for path in args:
-        for (dirpath, dirnames, filenames) in os.walk(path):
-            for filename in filenames:
-                yield os.path.join(dirpath, filename)
+procdraw.cpplint(procdraw.src_files())
 
-def exec_command(args):
-    returncode = subprocess.call(args)
-    if returncode != 0:
-        sys.exit(returncode)
-
-def src_files():
-    return (f for f in find('src', 'tests')
-            if f.endswith('.cpp') or f.endswith('.h'))
-
-def function_specs():
-    return (f for f in find('Documentation/docs/functions')
-            if f.endswith('.xml'))
-
-cpplint = 'third_party/google/styleguide/cpplint/cpplint.py'
-exec_command(itertools.chain(['python', cpplint], src_files()))
-
-schema = 'Documentation/docs/functions/function-spec.rnc'
-exec_command(itertools.chain(['jing', '-c', schema], function_specs()))
+procdraw.jing(procdraw.function_schema, procdraw.function_specs())
---
+++ scripts/procdraw.py
@@ -0,0 +1,33 @@
+import itertools, os, subprocess
+
+functions_src_dir = os.path.join('Documentation', 'docs', 'functions')
+function_schema = os.path.join(functions_src_dir, 'function-spec.rnc')
+
+cpplint_prog = os.path.join('third_party', 'google', 'styleguide',
+                            'cpplint', 'cpplint.py')
+
+def find(*args):
+    for path in args:
+        for (dirpath, dirnames, filenames) in os.walk(path):
+            for filename in filenames:
+                yield os.path.join(dirpath, filename)
+
+def exec_command(args):
+    returncode = subprocess.call(args)
+    if returncode != 0:
+        raise Exception('Error {:d}'.format(returncode))
+
+def is_cpp_file(filename):
+    return filename.endswith('.cpp') or filename.endswith('.h')
+
+def src_files():
+    return (f for f in find('src', 'tests') if is_cpp_file(f))
+
+def function_specs():
+    return (f for f in find(functions_src_dir) if f.endswith('.xml'))
+
+def cpplint(files):
+    exec_command(itertools.chain(['python', cpplint_prog], files))
+
+def jing(schema, files):
+    exec_command(itertools.chain(['jing', '-c', schema], files))
