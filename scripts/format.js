fa08c8f2214a4d085c6e0434bc2f61c05b9927b2
Simon Bates <simon@bitstructures.com>
2017-04-09 12:53:27 -0400

    Remove Node.js scripts

--- scripts/format.js
+++
@@ -1,7 +0,0 @@
-var shell = require("shelljs"),
-    Procdraw = require("./procdraw.js"),
-    procdraw = new Procdraw(shell);
-
-shell.config.fatal = true;
-
-procdraw.clangFormat(procdraw.srcFiles());
--- scripts/gl3w.js
+++
@@ -1,7 +0,0 @@
-var shell = require("shelljs");
-
-shell.config.fatal = true;
-
-shell.mkdir("-p", "third_party/gl3w_out");
-shell.cd("third_party/gl3w_out");
-shell.exec("python ../gl3w/gl3w_gen.py");
