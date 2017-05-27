8c47f86de86525ee3cdc128c47574670a25f2e4b
Simon Bates <simon@bitstructures.com>
2017-05-27 10:57:59 -0400

    Use system FreeType 2

--- third_party/freetype2-custom/ft2build.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROCDRAW_FT2BUILD_H_
-#define PROCDRAW_FT2BUILD_H_
-
-#define FT_CONFIG_OPTIONS_H <procdraw_ftoption.h>
-#define FT_CONFIG_MODULES_H <procdraw_ftmodule.h>
-
-#include <freetype/config/ftheader.h>
-
-#endif /* PROCDRAW_FT2BUILD_H_ */
