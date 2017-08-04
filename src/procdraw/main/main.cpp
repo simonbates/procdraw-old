3812b60e5e6654b4b02b9bb369c639d6cb6b7c30
Simon Bates <simon@bitstructures.com>
2017-08-04 10:38:13 -0300

    GH-89: Remove Windows code from main.cpp

--- src/procdraw/main/main.cpp
+++ src/procdraw/main/main.cpp
@@ -1,9 +1,5 @@
 #define SDL_MAIN_HANDLED
 
-#ifdef _WIN32
-#include <windows.h>
-#endif
-
 #include "procdraw/graphics/sdl_util.h"
 #include "procdraw/procdraw_app.h"
 
@@ -13,14 +9,3 @@ int main()
     procdraw::ProcdrawApp app;
     return app.MainLoop();
 }
-
-#ifdef _WIN32
-int CALLBACK WinMain(_In_ HINSTANCE hInstance,
-                     _In_ HINSTANCE hPrevInstance,
-                     _In_ LPSTR lpCmdLine,
-                     _In_ int nCmdShow)
-{
-    // Delegate to the main() function
-    return main();
-}
-#endif
