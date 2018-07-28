f7bb64e8c45aaed08798920dc968fae4b735c09a
Simon Bates <simon@bitstructures.com>
2018-07-28 19:58:16 -0400

    Continue port of InterpreterTests

--- tests/InterpreterTests.cpp
+++ tests/InterpreterTests.cpp
@@ -73,35 +73,35 @@ public:
     {
         Interpreter interpreter;
         AssertStackSize(0, interpreter);
-        interpreter.PushNil();                          // nil
+        interpreter.PushNil();                  // nil
         AssertStackSize(1, interpreter);
-        interpreter.PushSymbol("bar");                  // nil bar
+        interpreter.PushSymbol("bar");          // nil bar
         AssertStackSize(2, interpreter);
-        interpreter.Cons();                             // (bar)
+        interpreter.Cons();                     // (bar)
         AssertStackSize(1, interpreter);
-        interpreter.PushSymbol("foo");                  // (bar) foo
+        interpreter.PushSymbol("foo");          // (bar) foo
         AssertStackSize(2, interpreter);
-        interpreter.Cons();                             // (foo bar)
+        interpreter.Cons();                     // (foo bar)
         AssertStackSize(1, interpreter);
         Assert::AreEqual(ObjType::ConsPtr, interpreter.Type());
-        interpreter.Dup();                              // (foo bar) (foo bar)
+        interpreter.Dup();                      // (foo bar) (foo bar)
         AssertStackSize(2, interpreter);
-        interpreter.Car();                              // (foo bar) foo
+        interpreter.Car();                      // (foo bar) foo
         AssertStackSize(2, interpreter);
         Assert::AreEqual(ObjType::SymbolPtr, interpreter.Type());
         Assert::AreEqual(std::string("foo"), interpreter.PopSymbol()); // (foo bar)
         AssertStackSize(1, interpreter);
         Assert::AreEqual(ObjType::ConsPtr, interpreter.Type());
-        interpreter.Cdr();                              // (bar)
+        interpreter.Cdr();                      // (bar)
         AssertStackSize(1, interpreter);
         Assert::AreEqual(ObjType::ConsPtr, interpreter.Type());
-        interpreter.Dup();                              // (bar) (bar)
+        interpreter.Dup();                      // (bar) (bar)
         AssertStackSize(2, interpreter);
-        interpreter.Car();                              // (bar) bar
+        interpreter.Car();                      // (bar) bar
         AssertStackSize(2, interpreter);
         Assert::AreEqual(std::string("bar"), interpreter.PopSymbol()); // (bar)
         AssertStackSize(1, interpreter);
-        interpreter.Cdr();                              // nil
+        interpreter.Cdr();                      // nil
         AssertStackSize(1, interpreter);
         Assert::IsTrue(interpreter.IsNull());
         interpreter.Drop();
@@ -124,139 +124,139 @@ public:
         AssertStackSize(0, interpreter);
     }
 
+    TEST_METHOD(SymbolsOnStack)
+    {
+        Interpreter interpreter;
+        AssertStackSize(0, interpreter);
+        interpreter.PushSymbol("foo");
+        AssertStackSize(1, interpreter);
+        interpreter.PushSymbol("bar");
+        AssertStackSize(2, interpreter);
+        Assert::AreEqual(ObjType::SymbolPtr, interpreter.Type());
+        Assert::AreEqual(std::string("bar"), interpreter.PopSymbol());
+        AssertStackSize(1, interpreter);
+        Assert::AreEqual(ObjType::SymbolPtr, interpreter.Type());
+        Assert::AreEqual(std::string("foo"), interpreter.PopSymbol());
+        AssertStackSize(0, interpreter);
+    }
+
+    TEST_METHOD(Add)
+    {
+        Interpreter interpreter;
+        AssertStackSize(0, interpreter);
+        interpreter.PushReal(1.25);
+        AssertStackSize(1, interpreter);
+        interpreter.PushReal(2.5);
+        AssertStackSize(2, interpreter);
+        interpreter.Add();
+        AssertStackSize(1, interpreter);
+        Assert::AreEqual(ObjType::Real, interpreter.Type());
+        Assert::AreEqual(3.75, interpreter.PopReal());
+        AssertStackSize(0, interpreter);
+    }
+
+    TEST_METHOD(Dup)
+    {
+        Interpreter interpreter;
+        AssertStackSize(0, interpreter);
+        interpreter.PushSymbol("foo");
+        AssertStackSize(1, interpreter);
+        interpreter.Dup();
+        AssertStackSize(2, interpreter);
+        interpreter.PushReal(42.0);
+        AssertStackSize(3, interpreter);
+        interpreter.Dup();
+        AssertStackSize(4, interpreter);
+        Assert::AreEqual(ObjType::Real, interpreter.Type());
+        Assert::AreEqual(42.0, interpreter.PopReal());
+        AssertStackSize(3, interpreter);
+        Assert::AreEqual(ObjType::Real, interpreter.Type());
+        Assert::AreEqual(42.0, interpreter.PopReal());
+        AssertStackSize(2, interpreter);
+        Assert::AreEqual(ObjType::SymbolPtr, interpreter.Type());
+        Assert::AreEqual(std::string("foo"), interpreter.PopSymbol());
+        AssertStackSize(1, interpreter);
+        Assert::AreEqual(ObjType::SymbolPtr, interpreter.Type());
+        Assert::AreEqual(std::string("foo"), interpreter.PopSymbol());
+        AssertStackSize(0, interpreter);
+    }
+
+    TEST_METHOD(EvalSumExpression)
+    {
+        Interpreter interpreter;
+        AssertStackSize(0, interpreter);
+        interpreter.Read("(+ 1 (+ 2 3))");
+        interpreter.Eval();
+        AssertStackSize(1, interpreter);
+        Assert::AreEqual(ObjType::Real, interpreter.Type());
+        Assert::AreEqual(6.0, interpreter.PopReal());
+    }
+
+    TEST_METHOD(LoadAndStoreGlobalBindings)
+    {
+        Interpreter interpreter;
+        AssertStackSize(0, interpreter);
+        interpreter.PushReal(42.0);
+        interpreter.PushSymbol("foo");
+        interpreter.Store();
+        AssertStackSize(1, interpreter);
+        Assert::AreEqual(42.0, interpreter.PopReal());
+        AssertStackSize(0, interpreter);
+        interpreter.PushSymbol("foo");
+        interpreter.Load();
+        AssertStackSize(1, interpreter);
+        Assert::AreEqual(ObjType::Real, interpreter.Type());
+        Assert::AreEqual(42.0, interpreter.PopReal());
+    }
+
+    TEST_METHOD(Next)
+    {
+        Interpreter interpreter;
+        AssertStackSize(0, interpreter);
+        interpreter.PushNil();                          // nil
+        interpreter.PushReal(3.0);                      // nil 3
+        interpreter.Cons();                             // (3)
+        interpreter.PushReal(2.0);                      // (3) 2
+        interpreter.Cons();                             // (2 3)
+        interpreter.PushReal(1.0);                      // (2 3) 1
+        interpreter.Cons();                             // (1 2 3)
+        AssertStackSize(1, interpreter);
+        interpreter.Next();                             // (2 3) 1
+        AssertStackSize(2, interpreter);
+        Assert::AreEqual(ObjType::Real, interpreter.Type());
+        Assert::AreEqual(1.0, interpreter.PopReal());   // (2 3)
+        interpreter.Next();                             // (3) 2
+        Assert::AreEqual(ObjType::Real, interpreter.Type());
+        Assert::AreEqual(2.0, interpreter.PopReal());   // (3)
+        interpreter.Next();                             // nil 3
+        Assert::AreEqual(ObjType::Real, interpreter.Type());
+        Assert::AreEqual(3.0, interpreter.PopReal());   // nil
+        AssertStackSize(1, interpreter);
+        Assert::IsTrue(interpreter.IsNull());
+    }
+
+    TEST_METHOD(Swap)
+    {
+        Interpreter interpreter;
+        AssertStackSize(0, interpreter);
+        interpreter.PushSymbol("foo");
+        AssertStackSize(1, interpreter);
+        interpreter.PushSymbol("bar");
+        AssertStackSize(2, interpreter);
+        interpreter.Swap();
+        AssertStackSize(2, interpreter);
+        Assert::AreEqual(std::string("foo"), interpreter.PopSymbol());
+        AssertStackSize(1, interpreter);
+        Assert::AreEqual(std::string("bar"), interpreter.PopSymbol());
+        AssertStackSize(0, interpreter);
+    }
+
 };
 
 }
 }
 
 /*
-
-TEST_METHOD("Interpreter stack should support symbols")
-{
-    procdraw::Interpreter interpreter;
-    AssertStackSize(, interpreter0);
-    interpreter.PushSymbol("foo");
-    AssertStackSize(, interpreter1);
-    interpreter.PushSymbol("bar");
-    AssertStackSize(, interpreter2);
-    Assert::AreEqual(interpreter.Type() == procdraw::ObjType::SymbolPtr);
-    Assert::AreEqual(interpreter.PopSymbol() == "bar");
-    AssertStackSize(, interpreter1);
-    Assert::AreEqual(interpreter.Type() == procdraw::ObjType::SymbolPtr);
-    Assert::AreEqual(interpreter.PopSymbol() == "foo");
-    AssertStackSize(, interpreter0);
-}
-
-TEST_METHOD("Add should add 2 reals")
-{
-    procdraw::Interpreter interpreter;
-    AssertStackSize(, interpreter0);
-    interpreter.PushReal(1.25);
-    AssertStackSize(, interpreter1);
-    interpreter.PushReal(2.5);
-    AssertStackSize(, interpreter2);
-    interpreter.Add();
-    AssertStackSize(, interpreter1);
-    Assert::AreEqual(interpreter.Type() == procdraw::ObjType::Real);
-    Assert::AreEqual(interpreter.PopReal() == 3.75);
-    AssertStackSize(, interpreter0);
-}
-
-TEST_METHOD("Dup should duplicate top of stack")
-{
-    procdraw::Interpreter interpreter;
-    AssertStackSize(, interpreter0);
-    interpreter.PushSymbol("foo");
-    AssertStackSize(, interpreter1);
-    interpreter.Dup();
-    AssertStackSize(, interpreter2);
-    interpreter.PushReal(42.0);
-    AssertStackSize(, interpreter3);
-    interpreter.Dup();
-    AssertStackSize(, interpreter4);
-    Assert::AreEqual(interpreter.Type() == procdraw::ObjType::Real);
-    Assert::AreEqual(interpreter.PopReal() == 42.0);
-    AssertStackSize(, interpreter3);
-    Assert::AreEqual(interpreter.Type() == procdraw::ObjType::Real);
-    Assert::AreEqual(interpreter.PopReal() == 42.0);
-    AssertStackSize(, interpreter2);
-    Assert::AreEqual(interpreter.Type() == procdraw::ObjType::SymbolPtr);
-    Assert::AreEqual(interpreter.PopSymbol() == "foo");
-    AssertStackSize(, interpreter1);
-    Assert::AreEqual(interpreter.Type() == procdraw::ObjType::SymbolPtr);
-    Assert::AreEqual(interpreter.PopSymbol() == "foo");
-    AssertStackSize(, interpreter0);
-}
-
-TEST_METHOD("Eval (+ 1 (+ 2 3)) should be 6")
-{
-    procdraw::Interpreter interpreter;
-    AssertStackSize(, interpreter0);
-    interpreter.Read("(+ 1 (+ 2 3))");
-    interpreter.Eval();
-    AssertStackSize(, interpreter1);
-    Assert::AreEqual(interpreter.Type() == procdraw::ObjType::Real);
-    Assert::AreEqual(interpreter.PopReal() == 6.0);
-}
-
-TEST_METHOD("Load and Store should provide global bindings")
-{
-    procdraw::Interpreter interpreter;
-    AssertStackSize(, interpreter0);
-    interpreter.PushReal(42.0);
-    interpreter.PushSymbol("foo");
-    interpreter.Store();
-    AssertStackSize(, interpreter1);
-    Assert::AreEqual(interpreter.PopReal() == 42.0);
-    AssertStackSize(, interpreter0);
-    interpreter.PushSymbol("foo");
-    interpreter.Load();
-    AssertStackSize(, interpreter1);
-    Assert::AreEqual(interpreter.Type() == procdraw::ObjType::Real);
-    Assert::AreEqual(interpreter.PopReal() == 42.0);
-}
-
-TEST_METHOD("Next should iterate through a list structure")
-{
-    procdraw::Interpreter interpreter;
-    AssertStackSize(, interpreter0);
-    interpreter.PushNil();                              // nil
-    interpreter.PushReal(3.0);                          // nil 3
-    interpreter.Cons();                                 // (3)
-    interpreter.PushReal(2.0);                          // (3) 2
-    interpreter.Cons();                                 // (2 3)
-    interpreter.PushReal(1.0);                          // (2 3) 1
-    interpreter.Cons();                                 // (1 2 3)
-    AssertStackSize(, interpreter1);
-    interpreter.Next();                                 // (2 3) 1
-    AssertStackSize(, interpreter2);
-    Assert::AreEqual(interpreter.Type() == procdraw::ObjType::Real);
-    Assert::AreEqual(interpreter.PopReal() == 1.0);              // (2 3)
-    interpreter.Next();                                 // (3) 2
-    Assert::AreEqual(interpreter.Type() == procdraw::ObjType::Real);
-    Assert::AreEqual(interpreter.PopReal() == 2.0);              // (3)
-    interpreter.Next();                                 // nil 3
-    Assert::AreEqual(interpreter.Type() == procdraw::ObjType::Real);
-    Assert::AreEqual(interpreter.PopReal() == 3.0);              // nil
-    AssertStackSize(, interpreter1);
-    Assert::AreEqual(interpreter.IsNull());
-}
-
-TEST_METHOD("Swap should swap the top 2 elements on the stack")
-{
-    procdraw::Interpreter interpreter;
-    AssertStackSize(, interpreter0);
-    interpreter.PushSymbol("foo");
-    AssertStackSize(, interpreter1);
-    interpreter.PushSymbol("bar");
-    AssertStackSize(, interpreter2);
-    interpreter.Swap();
-    AssertStackSize(, interpreter2);
-    Assert::AreEqual(interpreter.PopSymbol() == "foo");
-    AssertStackSize(, interpreter1);
-    Assert::AreEqual(interpreter.PopSymbol() == "bar");
-    AssertStackSize(, interpreter0);
-}
 
 TEST_METHOD("Assoc(((a . 10) (b . 11) (c . 12)), b) should be (b . 11)")
 {
