fbbaec2ba7eeb8284c8263c201fe5c579908c108
Simon Bates <simon@bitstructures.com>
2019-05-31 17:53:08 -0400

    Remove old stdafx precompiled headers

--- src/Reader.cpp
+++ src/Reader.cpp
@@ -1,9 +1,8 @@
-/* Copyright (c) 2018 Simon Bates
+/* Copyright (c) 2018, 2019 Simon Bates
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "stdafx.h"
 #include "Reader.h"
 #include "Interpreter.h"
 #include <cctype>
